8344fe75c7403002c1d003cd6d0571ec2e5f9ecb
ThangHH <thang@canary-software.com>
2019-01-07 15:54:44 +0700

    add table and seeder

---
+++ database/migrations/2019_01_07_083022_create_bus_stop_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusStopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus_stop', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('location_x');
+            $table->string('location_y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus_stop');
+    }
+}
